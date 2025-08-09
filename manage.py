a56e76872295d7529d7021475ebc76fe2a56262d
choyeon-hong <79977896+choyeon-hong@users.noreply.github.com>
2025-08-09 16:29:18 +0100

    add collectstatic

--- manage.py
+++ manage.py
@@ -6,7 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "audir_project.settings")
+    settings_module = 'audir_project.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'audir_project.settings'
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
